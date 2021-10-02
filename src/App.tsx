62ac956018e61acfc571866e874a221ccb833692
thedon122 <donovan122@yahoo.com>
2021-10-01 23:54:21 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import './App.css';
-import Nav from "./components/Nav";
-import Menu from './components/Menu';
-import Products from './admin/Products';
+import Products from "./admin/Products";
 import {BrowserRouter, Route} from "react-router-dom";
 import Main from "./main/Main";
+import ProductsCreate from "./admin/ProductsCreate";
+import ProductsEdit from "./admin/ProductsEdit";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
+    return (
+        <div className="App">
+            <BrowserRouter>
                 <Route path='/' exact component={Main}/>
                 <Route path='/admin/products' exact component={Products}/>
-      </BrowserRouter>
-      <Nav />
+                <Route path='/admin/products/create' exact component={ProductsCreate}/>
+                <Route path='/admin/products/:id/edit' exact component={ProductsEdit}/>
+            </BrowserRouter>
 
-<div className="container-fluid">
-  <div className="row">
-    <Menu />
-    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div className="chartjs-size-monitor"><div className="chartjs-size-monitor-expand"><div className=""></div></div><div className="chartjs-size-monitor-shrink"><div className=""></div></div></div>
-      <Products />
-    </main>
-  </div>
-</div>
-</div>
-  );
+        </div>
+    );
 }
 
 export default App;
